467227eceebe6f8f3d2e81ea8274477771fbc154
sakhilpratap620 <you@example.com>
2023-02-28 08:06:23 +0530

    db files updated

--- src/insurence_premium/cassandra_db/cassandra_db.py
+++ src/insurence_premium/cassandra_db/cassandra_db.py
@@ -1,12 +1,13 @@
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
-from insurence_premium.util.util import read_yaml
+from insurence_premium.util.common import read_yaml
 from insurence_premium.constant import *
 from insurence_premium.exception import PremiumException
-from insurence_premium.logger import logging
+from insurence_premium import logger
 import pandas as pd
 import csv
-
+import sys , os
+from pathlib import Path
 
 
 
@@ -18,13 +19,13 @@ class CassandraDatabaseManager:
     CassandraDatabaseManager is high level api.
     """
 
-    def __init__(self , db_file_path:str):
+    def __init__(self , db_file_path:Path):
         try:
 
             self.table="insurance"
             self.key_space ="new_row"
 
-            self.db_info=read_yaml(file_path=db_file_path)
+            self.db_info=read_yaml(path_to_yaml=db_file_path)
 
         except Exception as e:
             raise PremiumException(e, sys) from e
@@ -33,11 +34,13 @@ class CassandraDatabaseManager:
         """
         try:
             self.db_info=self.db_info
+            logger.info("try to connect with db")
             cloud_config= {
             'secure_connect_bundle': self.db_info[SECRET_BUNDLE_KEY]
             }
             auth_provider = PlainTextAuthProvider(self.db_info[CLIENT_ID_KEY], self.db_info[CLIENT_SECRET_KEY])
             cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+            logger.info("db connection successfully stablish")
             session = cluster.connect("new_row")
 
             row=session.execute('SELECT * FROM insurance')
@@ -46,10 +49,10 @@ class CassandraDatabaseManager:
             if row:
                 for i in row:
                     self.a.append(i)
-            logging.info("data store in list successfully")
+            logger.info("data store in list successfully")
             df=pd.DataFrame(self.a)
             return df  
 
-            logging.info("df is found")     
-
-      
+            logger.info("data found successfully")
+        except Exception as e:
+            raise PremiumException(e,sys) from e
