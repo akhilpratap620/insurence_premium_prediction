00defbd3d35a047e4754c57bc0b00420aaac29a8
sakhilpratap620 <you@example.com>
2023-03-01 09:28:42 +0530

    some modification for data ingestion stage

--- src/insurence_premium/pipeline/stage_01_data_ingestion.py
+++ src/insurence_premium/pipeline/stage_01_data_ingestion.py
@@ -1,19 +1,20 @@
 from insurence_premium.exception import PremiumException
-import os,sys
+import os, sys
 from insurence_premium.config import ConfigurationManager
 from insurence_premium.components.data_ingestion import DataIngestion
+
+
 def main():
-    
-    con_obj=ConfigurationManager()
-    data_ingestion_config=con_obj.get_data_ingestion_config()
-    data_ingestion =DataIngestion(config=data_ingestion_config)
+
+    con_obj = ConfigurationManager()
+    data_ingestion_config = con_obj.get_data_ingestion_config()
+    data_ingestion = DataIngestion(config=data_ingestion_config)
     data_ingestion.download_data()
     data_ingestion.split_data_as_train_test()
 
 
-if  __name__=='__main__':
+if __name__ == "__main__":
     try:
-        
         main()
     except Exception as e:
-        raise PremiumException(e,sys) from e    
+        raise PremiumException(e, sys) from e
