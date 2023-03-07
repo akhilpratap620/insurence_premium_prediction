8c6d223ed58dbe96f20b01c17eace1cec91159bc
sakhilpratap620 <you@example.com>
2023-03-07 08:44:06 +0530

    cassandra_db file

--- src/insurence_premium/cassandra_db/cassandra_db.py
+++ src/insurence_premium/cassandra_db/cassandra_db.py
@@ -2,15 +2,10 @@ from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 from insurence_premium.util.common import read_yaml
 from insurence_premium.constant import *
-from insurence_premium.exception import PremiumException
 from insurence_premium import logger
 import pandas as pd
-import csv
-import sys , os
+import sys
 from pathlib import Path
-
-
-
 
 
 class CassandraDatabaseManager:
@@ -19,40 +14,40 @@ class CassandraDatabaseManager:
     CassandraDatabaseManager is high level api.
     """
 
-    def __init__(self , db_file_path:Path):
+    def __init__(self, db_file_path: Path):
         try:
 
-            self.table="insurance"
-            self.key_space ="new_row"
+            self.table = "insurance"
+            self.key_space = "new_row"
 
-            self.db_info=read_yaml(path_to_yaml=db_file_path)
+            self.db_info = read_yaml(path_to_yaml=db_file_path)
 
         except Exception as e:
-            raise PremiumException(e, sys) from e
+            raise e
+
     def data_finder(self):
-        """data finder will help to fetch data from cassandra database
-        """
+        """data finder will help to fetch data from cassandra database"""
         try:
-            self.db_info=self.db_info
+            self.db_info = self.db_info
             logger.info("try to connect with db")
-            cloud_config= {
-            'secure_connect_bundle': self.db_info[SECRET_BUNDLE_KEY]
-            }
-            auth_provider = PlainTextAuthProvider(self.db_info[CLIENT_ID_KEY], self.db_info[CLIENT_SECRET_KEY])
+            cloud_config = {"secure_connect_bundle": self.db_info[SECRET_BUNDLE_KEY]}
+            auth_provider = PlainTextAuthProvider(
+                self.db_info[CLIENT_ID_KEY], self.db_info[CLIENT_SECRET_KEY]
+            )
             cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
             logger.info("db connection successfully stablish")
             session = cluster.connect("new_row")
 
-            row=session.execute('SELECT * FROM insurance')
-            self.a=[]
+            row = session.execute("SELECT * FROM insurance")
+            self.a = []
 
             if row:
                 for i in row:
                     self.a.append(i)
             logger.info("data store in list successfully")
-            df=pd.DataFrame(self.a)
-            return df  
+            df = pd.DataFrame(self.a)
+            return df
 
             logger.info("data found successfully")
         except Exception as e:
-            raise PremiumException(e,sys) from e
+            raise e
