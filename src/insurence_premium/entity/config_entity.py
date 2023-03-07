857669865a2dd90aabe0d7c30526fc5e4532a284
sakhilpratap620 <you@example.com>
2023-03-07 08:45:09 +0530

    entity update

--- src/insurence_premium/entity/config_entity.py
+++ src/insurence_premium/entity/config_entity.py
@@ -7,3 +7,11 @@ class DataIngestionConfig:
     raw_data: Path
     ingested_train_dir: Path
     ingested_test_dir: Path
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    schema_file_path:Path
+    report_file_path: Path
+    report_page_file_path: Path
+    
