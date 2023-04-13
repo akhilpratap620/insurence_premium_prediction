645795936b77ead9b9092b084a91cca8882209e9
sakhilpratap620 <sakhilpratap620@gmail.com>
2023-04-13 22:37:33 +0530

    app.py created successfully

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 import os
-from flask import Flask
+from flask import Flask ,request, render_template
+import numpy as np
+import pandas as pd 
+from sklearn.preprocessing import StandardScaler
+from insurence_premium.pipeline.prediction_pipeline import CustomData ,PredictPipeline
 
 app = Flask(__name__)
 
+#route for home page
+
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['GET','POST'])
+def prediction_data():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            age=request.form.get('age'),
+            sex=request.form.get('sex'),
+            bmi=request.form.get('bmi'), 
+            children=request.form.get('children'),
+            smoker=request.form.get('smoker'),
+            region=request.form.get('region')
+            )
+        pred_df=data.get_data_as_frame()
+        print(pred_df)
+        prediction_pipeline=PredictPipeline()
+        results=prediction_pipeline.predict(pred_df)
+        return render_template('home.html' ,results=results[0]
+        )
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
---
+++ src/insurence_premium/pipeline/prediction_pipeline.py
@@ -0,0 +1,49 @@
+import os
+from insurence_premium import logger
+from insurence_premium.util import load_object
+from insurence_premium.config import ConfigurationManager
+import pandas as pd
+
+class PredictPipeline:
+    def __init__(self):
+        self.config=ConfigurationManager()
+        self.model_trainer_config=self.config.get_model_trainer_config()
+        self.model_transform_config=self.config.get_data_transformation_config()
+
+
+    def predict(self,features):
+        try:
+            model_path=self.model_trainer_config.trained_model_file_path
+            preprocessor_path=self.model_transform_config.preprocessing_file_path
+            model=load_object(file_path=model_path)
+            
+            preds=model.predict(features)
+            return preds
+        except Exception as e:
+            raise e    
+
+class CustomData:
+
+    def __init__(self,age: int,sex: str,bmi: float,children: int,smoker: str,region: str):
+        self.age=age
+        self.sex=sex
+        self.bmi=bmi
+        self.children=children
+        self.smoker=smoker
+        self.region=region
+
+    def get_data_as_frame(self):
+        try:
+            custom_data={
+                "age":[self.age],
+                "sex":[self.sex],
+                "bmi":[self.bmi],
+                "children":[self.children],
+                "smoker":[self.smoker],
+                "region":[self.region]
+            }
+            return pd.DataFrame(custom_data)
+        except Exception as e:
+            raise e    
+    
+    
