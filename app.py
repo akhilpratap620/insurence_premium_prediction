4617b5a2abe1303673a2d3ae1265c7b5a092c6ee
sakhilpratap620 <sakhilpratap620@gmail.com>
2023-04-28 00:05:39 +0530

    app.py update

--- app.py
+++ app.py
@@ -37,4 +37,5 @@ def prediction_data():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     
-    app.run(host='0.0.0.0')  
+    app.run(host='0.0.0.0', port=8080)  
+    
